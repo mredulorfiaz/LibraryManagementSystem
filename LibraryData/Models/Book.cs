09dd7c081a6cf8503d4fac990d0f814032552abc
Orfiaj <orfiajkarim@gmail.com>
2021-01-13 10:29:06 +0600

    Added New Models

---
+++ LibraryData/Models/Book.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace LibraryData.Models
+{
+    public class Book : LibraryAsset
+    {
+        [Required] [Display(Name = "ISBN #")] public string ISBN { get; set; }
+        [Required] public string Author { get; set; }
+        [Required] [Display(Name = "DDC")] public string BookIndex { get; set; }
+    }
+}
--- LibraryData/Models/Member.cs
+++ LibraryData/Models/Member.cs
@@ -6,11 +6,28 @@ namespace LibraryData.Models
 {
     public class Member
     {
-        public int Id { get; set; }
+          public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        [StringLength(30, ErrorMessage = "Limit first name to 30 characters.")]
         public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        [StringLength(30, ErrorMessage = "Limit last name to 30 characters.")]
         public string LastName { get; set; }
-        public string Address { get; set; }
-        public DateTime DateOfBirth { get; set; }
-        public string  PhoneNo { get; set; }
+
+        [Required] public string Address { get; set; }
+
+        [Required] public DateTime DateOfBirth { get; set; }
+
+        public string Telephone { get; set; }
+        public string Gender { get; set; }
+
+        [Required]
+        [Display(Name = "Library Card")]
+        public LibraryCard LibraryCard { get; set; }
+        public LibraryBranch HomeLibraryBranch { get; set; }
     }
 }
